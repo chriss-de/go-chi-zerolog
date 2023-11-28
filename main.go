933fb7c3183ad7aa995f22afb8c3fcc6f0ad73f6
Christoph Hartmann <christoph.hartmann@aeb.com>
2023-11-28 09:49:41 +0100

    made it better for me

--- main.go
+++ main.go
@@ -1,18 +1,35 @@
 package chizerolog
 
 import (
+	"github.com/rs/zerolog/log"
+	"net"
 	"net/http"
 	"runtime/debug"
+	"strings"
 	"time"
 
-	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/rs/zerolog"
 )
 
-func LoggerMiddleware(logger *zerolog.Logger) func(next http.Handler) http.Handler {
+type LoggerOpts struct {
+	AccessLogTypeName       string
+	PrintLogType            bool
+	PrintStackTraceToStderr bool
+}
+
+func LoggerMiddleware(logger *zerolog.Logger, opts *LoggerOpts) func(next http.Handler) http.Handler {
+	if opts == nil {
+		opts = &LoggerOpts{
+			AccessLogTypeName:       "access",
+			PrintLogType:            true,
+			PrintStackTraceToStderr: true,
+		}
+	}
+
 	return func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
-			log := logger.With().Logger()
+			zlog := logger.With().Logger()
 
 			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
 
@@ -22,27 +39,35 @@ func LoggerMiddleware(logger *zerolog.Logger) func(next http.Handler) http.Handl
 
 				// Recover and record stack traces in case of a panic
 				if rec := recover(); rec != nil {
-					log.Error().
-						Str("type", "error").
-						Timestamp().
-						Interface("recover_info", rec).
-						Bytes("debug_stack", debug.Stack()).
-						Msg("log system error")
+					if opts.PrintStackTraceToStderr {
+						debug.PrintStack()
+					} else {
+						le := zlog.Error()
+						if opts != nil && opts.PrintLogType && opts.AccessLogTypeName != "" {
+							le = le.Str("type", "error")
+						}
+						le.Timestamp().
+							Interface("recover_info", rec).
+							Bytes("debug_stack", debug.Stack()).
+							Msg("log system error")
+					}
 					http.Error(ww, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 				}
 
 				// log end request
-				log.Info().
-					Str("type", "access").
-					Timestamp().
+				le := zlog.Info()
+				if opts != nil && opts.PrintLogType && opts.AccessLogTypeName != "" {
+					le = le.Str("type", opts.AccessLogTypeName)
+				}
+				le.Timestamp().
 					Fields(map[string]interface{}{
-						"remote_ip":  r.RemoteAddr,
+						"remote_ip":  getRemoteAddr(r),
 						"url":        r.URL.Path,
 						"proto":      r.Proto,
 						"method":     r.Method,
 						"user_agent": r.Header.Get("User-Agent"),
 						"status":     ww.Status(),
-						"latency_ms": float64(t2.Sub(t1).Nanoseconds()) / 1000000.0,
+						"latency":    t2.Sub(t1).Truncate(1000 * time.Nanosecond).String(),
 						"bytes_in":   r.Header.Get("Content-Length"),
 						"bytes_out":  ww.BytesWritten(),
 					}).
@@ -53,4 +78,16 @@ func LoggerMiddleware(logger *zerolog.Logger) func(next http.Handler) http.Handl
 		}
 		return http.HandlerFunc(fn)
 	}
+}
+
+func getRemoteAddr(r *http.Request) string {
+	remoteAddr := r.RemoteAddr
+	if strings.Contains(remoteAddr, ":") {
+		var err error
+		remoteAddr, _, err = net.SplitHostPort(remoteAddr)
+		if err != nil {
+			log.Error().Err(err).Msg("could not parse remote address")
+		}
+	}
+	return remoteAddr
 }
